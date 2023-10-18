d9ec8f1392a310389d4a9abc0d5a1c52ada13a73
Luis Silva <luansimo@outlook.com>
2023-10-17 23:01:06 -0600

    Better urls fix

--- saes/urls.py
+++ saes/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from apps.fireapp import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fireapp/', views.index, name='fireapp/index'),
+    path('fireapp/', include("apps.fireapp.urls")),
 ]
