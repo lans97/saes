10aac600301fe228f6f49ac7d88c3c5e99b52b3b
Luis Silva <luis.silva1mon@gmail.com>
2023-10-29 14:22:10 -0600

    Updated settings, added bootstrap navbar

--- saes/settings.py
+++ saes/settings.py
@@ -11,23 +11,33 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+from os.path import abspath, basename, dirname, join, normpath
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_TEMPLATES = [
+    join(BASE_DIR, 'templates'),
+]
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6(qoifx%mzurwejs!7dl91%y#av$y!47dt%z%zl3$rc4c1qcz4'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    join(BASE_DIR, "static"),
+]
+
+SECRET_FILE = normpath(join(BASE_DIR, 'secrets', 'SECRET.key'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 
 # Application definition
@@ -57,7 +67,7 @@ ROOT_URLCONF = 'saes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': PROJECT_TEMPLATES,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,19 +124,19 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# finally grab the SECRET KEY
+try:
+    SECRET_KEY = open(SECRET_FILE).read().strip()
+except IOError:
+    try:
+        from django.core.management.utils import get_random_secret_key
+        SECRET_KEY = get_random_secret_key()
+        with open(SECRET_FILE, 'w') as f:
+            f.write(SECRET_KEY)
+    except IOError:
+        raise Exception('Could not open %s for writing!' % SECRET_FILE)
