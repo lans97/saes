d87ac3ed9244828974f990c20dc302d84a5ac2e8
Luis Silva <luis.silva1mon@gmail.com>
2023-11-14 14:22:40 -0600

    plot examlpe at /example

--- apps/fireapp/models.py
+++ apps/fireapp/models.py
@@ -14,3 +14,4 @@ class Sensor(models.Model):
 
     def __str__(self):
         return self.name
+
--- apps/fireapp/views.py
+++ apps/fireapp/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .models import Sensor
 from .forms import SignUpForm, SensorForm
 
-# Create your views here.
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+from firebase_admin import db
 
 def landing(request):
     return render(request, 'landing/home.html')
@@ -77,13 +81,37 @@ def add_sensor_view(request):
 
 @login_required
 def dashboard_view(request, sensor_id):
+    ref = db.reference('/')
+    ref.set(value)
     context = {}
     context['sensor_id'] = sensor_id
     return render(request, 'fireapp/dashboard.html', context)
-
 
 def logout_view(request):
     logout(request)
     next_url = request.POST.get('next') or request.GET.get('next') or 'landing-home'
     return redirect(next_url)
 
+def plot_ex(request):
+    # Your data for the plot
+    x = [1, 2, 3, 4, 5]
+    y = [10, 20, 15, 25, 30]
+
+    # Create the plot
+    plt.plot(x, y)
+    plt.xlabel('X-axis label')
+    plt.ylabel('Y-axis label')
+    plt.title('My Plot')
+
+    # Save the plot to a BytesIO object
+    image_stream = BytesIO()
+    plt.savefig(image_stream, format='png')
+    image_stream.seek(0)
+    plt.close()
+
+    # Convert the BytesIO object to a base64-encoded string
+    image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
+
+    # Pass the base64 string to the template
+    context = {'image_base64': image_base64}
+    return render(request, 'fireapp/example.html', context)
